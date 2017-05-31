7995eca29963d9c25b6672527d96b7901f6e9805
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-05-31 14:19:11 -0600

    final commit

--- lib/markdown_parser.rb
+++ lib/markdown_parser.rb
@@ -2,9 +2,13 @@
 class MarkdownParser
 
   def parse(new_line_text)
-    if new_line_text
+    if new_line_text.include?("\n") == false
       new_line_text.insert(0, "<p>")
       new_line_text.insert(-1, "</p>")
+    elsif new_line_text.include?("\n") && new_line_text[-2] != '\n'
+      new_line_text.insert(0, "<p>")
+      new_line_text.insert(-1, "</p>")
+    elsif new_line_text.include?("\n\n") && new_line_text
     end
     new_line_text
   end
--- test/markdown_parser_test.rb
+++ test/markdown_parser_test.rb
@@ -5,11 +5,24 @@ require './lib/markdown_parser'
 
 class MarkdownParserTest < Minitest::Test
 
-  def test_it_splits_at_new_line
+  def test_it_wraps_paragraphs_with_p
     mp = MarkdownParser.new
     result = mp.parse("This is the first line of the paragraph.")
     expected = "<p>This is the first line of the paragraph.</p>"
     assert_equal expected, result
   end
 
+  def test_it_splits_at_new_line
+    mp = MarkdownParser.new
+    result = mp.parse("This is the first line of the paragraph.\nThis is the second line of the same paragraph.")
+    expected = "<p>This is the first line of the paragraph.\nThis is the second line of the same paragraph.</p>"
+    assert_equal expected, result
+  end
+
+  def test_multiple_line_paragraphs
+    mp = MarkdownParser.new
+    result = mp.parse("This is the first line of the first paragraph.\n\nThis is the first line of the second paragraph.")
+    expected = "<p>This is the first line of the frist paragraph.</p>\n\n<p>This it the first line of the second paragraph.</p>"
+    assert_equal expected, result
+  end
 end
